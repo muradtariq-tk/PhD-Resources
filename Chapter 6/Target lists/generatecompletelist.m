c88bff6ae9f46922239f0643ca32c3b028ea4a75
muradtariq <muradtariq.tk@gmail.com>
2017-09-17 19:04:39 +0800

    chap 5 code

--- Chapter 6/Target lists/generatecompletelist.m
+++ Chapter 6/Target lists/generatecompletelist.m
@@ -1,5 +1,11 @@
 function [ret] = generatecompletelist(sch,a,b,c,d,e,f,ml,rlist)
-
+ll = {};
+ll{1}=a;
+ll{2}=b;
+ll{3}=c;
+ll{4}=d;
+ll{5}=e;
+ll{6}=f;
 ind = ml(:,1);
 [x y] = size(ind);
 indx(:,1) =  ismember(ind,a(:,1));
@@ -14,8 +20,13 @@ ret = zeros(x,1);
 for i  = 1:1:6
     for j = 1:1:x
         if ret(j,1)==0
-            selectedlist = indx(:,rlist(i,sch));
-            ret(j,1)= ml(j,1)*selectedlist(j,1);
+            selectedlistind = indx(:,rlist(i,sch));
+            movieid= ml(j,1)*selectedlistind(j,1);
+            selectedlist = ll{rlist(i,sch)};
+            if movieid ~=0
+                loc=find(selectedlist(:,1)==movieid);
+                ret(j,1)=selectedlist(loc,2);
+            end
         end
     end
 end
